4fe36c42428564afe6a68f8d65725ae8e669a742
SahilGidwani <sahilgidwani98@gmail.com>
2024-04-29 15:24:47 +0530

    User list project implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react"
+import { User } from "./User"
+
 function App() {
-  return <h1>Hello World</h1>
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const controller = new AbortController()
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data)
+      })
+      .catch((e) => {
+        console.log("Error fetching data: ", e)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+  return (
+    <>
+      <h1>User List:</h1>
+      {isLoading ? (
+        <h2>Loading ....</h2>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} user={user} />
+          })}
+        </ul>
+      )}
+    </>
+  )
 }
 
 export default App
