70033f2ad92b5b9fbfd27b9ee7b0fff015a1d7c2
SahilGidwani <sahilgidwani98@gmail.com>
2024-05-01 10:32:36 +0530

    useLocalStorage custom hook project implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,67 @@
+import { useLocalStorage } from "./useLocalStorage"
+
 function App() {
-  return <h1>Hello World</h1>
+  const [firstName, setFirstName] = useLocalStorage("FIRST_NAME", "")
+
+  // Bonus:
+  const [lastName, setLastName] = useLocalStorage("LAST_NAME", () => {
+    return "Default"
+  })
+
+  // Bonus:
+  const [hobbies, setHobbies] = useLocalStorage("HOBBIES", [
+    "Programming",
+    "Weight Lifting",
+  ])
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>First Name</label>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+
+      {/* Bonus: */}
+      {
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginBottom: "1rem",
+          }}
+        >
+          <label>Last Name</label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+      }
+
+      {/* Bonus: */}
+      <div>{hobbies.join(", ")}</div>
+      <button
+        onClick={() =>
+          setHobbies((currentHobbies) => [...currentHobbies, "New Hobby"])
+        }
+      >
+        Add Hobby
+      </button>
+    </>
+  )
 }
 
 export default App
